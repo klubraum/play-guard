d254a535144063e7d12e030568790260588d5ad0
Steffen Haak <haak@aucentiq.com>
2023-07-04 10:06:58 +0200

    fix: tests

--- module/test/com/digitaltangible/playguard/XForwardedTrustImmediateConnectionRequestHandlerSpec.scala
+++ module/test/com/digitaltangible/playguard/XForwardedTrustImmediateConnectionRequestHandlerSpec.scala
@@ -2,16 +2,18 @@ package com.digitaltangible.playguard
 
 import org.scalatest.matchers.must.Matchers
 import org.scalatestplus.play.PlaySpec
-import play.api.http.{ DefaultHttpErrorHandler, HttpConfiguration }
+import play.api.http.{DefaultHttpErrorHandler, HttpConfiguration}
 import play.api.mvc.Headers
 import play.api.mvc.request.RemoteConnection
 import play.api.routing.Router
 import play.api.test.FakeRequest
 
+import javax.inject.Provider
+
 class XForwardedTrustImmediateConnectionRequestHandlerSpec extends PlaySpec with Matchers {
 
   val handler = new XForwardedTrustImmediateConnectionRequestHandler(
-    Router.empty,
+    () => Router.empty,
     DefaultHttpErrorHandler,
     HttpConfiguration(),
     Nil
